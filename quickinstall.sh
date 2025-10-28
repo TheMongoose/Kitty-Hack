87139152c728ebab6252ec978751c56c9644a362
Konrad Mohr <konradmohr1@gmail.com>
2025-10-28 13:11:25 -0400

    Update quickinstall.sh

--- quickinstall.sh
+++ quickinstall.sh
@@ -1,7 +1,5 @@
 !#/bin/bash
-sudo apt install python3-full git
-git clone https://github.com/TheMongoose/Kitty-Hack
-cd Kitty-Hack
+sudo apt install python3-full
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
